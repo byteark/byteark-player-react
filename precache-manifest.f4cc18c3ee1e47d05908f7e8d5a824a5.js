37de5e8d96bb41b43631ddcd7f7c71e3c6f97d9d
Jatesadakarn Saengrat <jatesadakarn@inox.co.th>
2020-04-27 17:14:15 +0700

    Updates

---
+++ precache-manifest.f4cc18c3ee1e47d05908f7e8d5a824a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53fc13f74c8d639319117772428c44d3",
+    "url": "/byteark-player-react/index.html"
+  },
+  {
+    "revision": "1ed515d2de09f3bd416a",
+    "url": "/byteark-player-react/static/css/main.dee1957f.chunk.css"
+  },
+  {
+    "revision": "f67566f8772b0e2df506",
+    "url": "/byteark-player-react/static/js/2.7cd36e98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/byteark-player-react/static/js/2.7cd36e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed515d2de09f3bd416a",
+    "url": "/byteark-player-react/static/js/main.31eb8027.chunk.js"
+  },
+  {
+    "revision": "a7a0a730eae9df986499",
+    "url": "/byteark-player-react/static/js/runtime-main.fae9a09b.js"
+  }
+]);
