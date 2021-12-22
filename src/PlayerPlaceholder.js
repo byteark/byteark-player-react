700c0da4024e1c597ee2a03153ed93ae56705dfd
Jatesadakarn Saengrat <jatesadakarn@inox.co.th>
2021-12-22 13:06:45 +0700

    Player placeholder will hide the big play button

--- src/PlayerPlaceholder.js
+++ src/PlayerPlaceholder.js
@@ -62,17 +62,21 @@ export default function PlayerPlaceholder(props) {
       className={props.className}
       style={placeholderCustomStyle}
     >
-      <svg
-        className='play-icon'
-        width='90'
-        viewBox='0 0 60 60'
-        style={playIconStyle}
-      >
-        <path
-          style={pathStyle}
-          d='M47.43,27.26,14.11,5.87A3.34,3.34,0,0,0,9,8.79V51.56a3.34,3.34,0,0,0,5.11,2.91L47.43,33.09A3.49,3.49,0,0,0,47.43,27.26Z'
-        />
-      </svg>
+      {
+        options.controls === undefined || options.controls == true
+          ? <svg
+            className='play-icon'
+            width='90'
+            viewBox='0 0 60 60'
+            style={playIconStyle}
+          >
+              <path
+                style={pathStyle}
+                d='M47.43,27.26,14.11,5.87A3.34,3.34,0,0,0,9,8.79V51.56a3.34,3.34,0,0,0,5.11,2.91L47.43,33.09A3.49,3.49,0,0,0,47.43,27.26Z'
+              />
+            </svg>
+          : null
+      }
       {props.error ? <PlayerLoadErrorMessage {...props.error} /> : null}
     </div>
   )
