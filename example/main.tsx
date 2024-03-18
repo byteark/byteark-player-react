463f796ae3bb3c167c5a0bda858440e4a159c4ea
nonth <nonthz@gmail.com>
2024-03-18 18:40:54 +0700

    Update example and vite config for deploy to github page

--- example/main.tsx
+++ example/main.tsx
@@ -4,9 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
+// https://stackoverflow.com/a/76622867
+const basename = import.meta.env.DEV ? undefined : '/byteark-player-react/'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.example.ts
+++ vite.config.example.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // https://vitejs.dev/guide/static-deploy#github-pages
+  base: '/byteark-player-react/',
   plugins: [react()],
   build: {
-    copyPublicDir: false
+    outDir: './dist/example'
   }
 })
